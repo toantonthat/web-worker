b6c985f5c59308d97d9e8c880937f7ec835c6f81
Toan Ton <tton@advisor360.com>
2024-07-08 01:49:30 +0700

    update code

--- src/js/app.worker.js
+++ src/js/app.worker.js
@@ -1,8 +1,6 @@
 console.log('load app.worker.js');
 
-self.onmessage = event => {
-  console.log('ToanDN received message: ', event.data);
-  const msg = 'Hello ToanTT';
-  console.log('ToanDN sends: ', msg);
-  self.postMessage(msg);
+self.onmessage = (event) => {
+  console.log('Worker received: ', event.data);
+  self.postMessage('hello, ToanTT')
 }
